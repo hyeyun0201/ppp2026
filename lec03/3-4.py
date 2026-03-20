64a679ad787c1413f889ea561d8128ef1588f518
hyeyun_0201 <hyeyun099@gmail.com>
2026-03-21 00:42:04 +0900

    Refactor circle calculations for precision formatting

--- lec03/3-4.py
+++ lec03/3-4.py
@@ -1,5 +1,5 @@
 import math
 R = float(input("반지름을 입력하시오 : "))
-C = round(2 * math.pi * R, 1)
-P = round(math.pi * R**2,2)
-print(f"원의 둘레는 {C}, 원의 면적은 {P}이다.")
+C = 2 * math.pi * R
+P = math.pi * R**2
+print(f"원의 둘레는 {C:.1f}, 원의 면적은 {P:.2f}이다.")
