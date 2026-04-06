cd9ffe09e034839b9aa2408fdcf66f885f8264d5
hyeyun_0201 <hyeyun099@gmail.com>
2026-04-06 13:58:19 +0900

    Delete lec04 directory

--- lec04/6-1.py
+++
@@ -1,3 +0,0 @@
-dan = int(input("몇 단을 출력할까요? : "))
-for n in range(1, 10):
-    print(f"{dan} X {n} = {n*dan}")
--- lec04/6-2.py
+++
@@ -1,3 +0,0 @@
-Star = int(input("몇 줄의 삼각형을 그릴까요? : "))
-for n in range(1, Star+1):
-    print(f"{"*"*n}")
