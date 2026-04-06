f3f14f2ed4d51cc57d74c0eb264989262b4feccc
hyeyun_0201 <hyeyun099@gmail.com>
2026-04-06 13:49:39 +0900

    Delete lec05/hw07 directory

--- lec05/hw07/7-1.py
+++
@@ -1,7 +0,0 @@
-cal_dict = {"한라봉": 50, "딸기": 34, "바나나": 77}
-eat_dict = {"한라봉": 100, "딸기": 200, "바나나": 500 }
-total_cal = 0
-for fruit, cal in eat_dict.items():
-    if fruit in cal_dict:
-        total_cal += cal * cal_dict[fruit]
-print(total_cal)
